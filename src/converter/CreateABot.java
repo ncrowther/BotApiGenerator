69996d3a45c3778a5ef61365b89f91f0a22edbe9
Nigel T. Crowther <ncrowther@uk.ibm.com>
2021-11-21 21:57:34 +0000

    Generate bot from chat bot

--- src/converter/CreateABot.java
+++ src/converter/CreateABot.java
@@ -18,76 +18,58 @@ import java.nio.file.Path;
 import java.util.Stack;
 
 import converter.bwl.BwlJsonParser;
-import converter.bwl.BwlRestApi;
 import converter.bpmn.BpmnTask;
 import converter.bpmn.IBpmnParser;
 import converter.openApi.OpenApiWriter;
 import converter.common.StringUtils;
 import converter.wal.WalWriter;
 
-public class BwlToOpenApi {
-	
+public class CreateABot {
+
 	public static void main(String[] args) {
 
-		String bwlProcessUrl= "";
-		String bwlUsername = "";
-		String bwlPassword = "";
+		String bpmnFile = "";
 		String baseDir = ".";
-		
+
 		System.out.println("Args: " + args.length);
-		
+
 		try {
 			if (args.length > 0) {
-				bwlProcessUrl = args[0];	
+				bpmnFile = args[0];
 				System.out.println("Arg[0]: " + args[0]);
 			} else {
-				throw new Exception("No URL specified");
-			}				
-			
+				throw new Exception("No file specified");
+			}
+
 			if (args.length > 1) {
-				bwlUsername = args[1];
+				baseDir = args[1];
 				System.out.println("Arg[1]: " + args[1]);
 			} else {
-				throw new Exception("No BWL Username supplied");
-			}	
-			
-			if (args.length > 2) {
-				bwlPassword = args[2];	
-				System.out.println("Arg[2]: " + args[2]);
-			} else {
-				throw new Exception("No BWL Password supplied");
-	
-			}	
-			
-			if (args.length > 3) {
-				baseDir = args[3];	
-				System.out.println("Arg[3]: " + args[3]);
-			} else {
 				throw new Exception("No base directory specified");
-			}			
+			}
 
-			System.out.println("Processing URL:" + bwlProcessUrl + " U:" + bwlUsername + " P:" + bwlPassword);
+			System.out.println("Processing :" + bpmnFile);
 
-			String jsonString = BwlRestApi.getJson(bwlProcessUrl, bwlUsername, bwlPassword);
-			
-			// System.out.println(jsonString);
+			Path path = Path.of(bpmnFile);
+			String jsonString = Files.readString(path, StandardCharsets.US_ASCII);
+
+			System.out.println(jsonString);
 
 			IBpmnParser bpmnParser = new BwlJsonParser(jsonString);
 
 			Stack<BpmnTask> taskIds = bpmnParser.getTaskIds();
-			
+
 			while (!taskIds.empty()) {
 				BpmnTask bpmnTask = taskIds.pop();
-				
 
 				final String generatedDir = baseDir + "\\generated\\";
-				String botFile =  generatedDir + StringUtils.convertToTitleCase(bpmnTask.getName());
+				String botFile = generatedDir + StringUtils.convertToTitleCase(bpmnTask.getName());
 				String walFileName = botFile + ".txt";
 				String openApiFileName = botFile + ".yaml";
-				
+
 				WalWriter.writeRPAFile(walFileName, bpmnTask);
 				OpenApiWriter.writeOpenApiFile(openApiFileName, bpmnTask);
-				
+
 				System.out.println("Code generated in " + walFileName);
 			}
 
